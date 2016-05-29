9d0bff080e56ef398d89e590fd17f990aa75c71e
Kizniche <KyleTGabriel@gmail.com>
2016-05-28 23:50:46 -0400

    use pi revision/model dictionary, minor updates

--- mycodo_stats_flask.py
+++ mycodo_stats_flask.py
@@ -29,16 +29,17 @@ def default_page():
         sort_type = request.form['sorttype']
     stats_data = get_stats_data()
     ids = get_ids(sort_type)
-
+    pi_versions = get_pi_versions()
     return render_template('mycodo_stats_view.html',
                            sort_type=sort_type,
                            stats_data=stats_data,
-                           ids=ids)
+                           ids=ids,
+                           pi_versions=pi_versions)
 
 
 def get_stats_data():
+    """Return all statistics data for the past 5 days"""
     dbcon = influx_db.connection
-
     raw_data = dbcon.query("""SELECT value
                               FROM /.*/
                               WHERE time > now() - 5d
@@ -49,7 +50,8 @@ def get_stats_data():
     return raw_data
 
 
-def get_ids(sort_name):
+def get_ids(measurement):
+    """Return a dictionary of user ids sorted based on measurement"""
     dbcon = influx_db.connection
     raw_data = dbcon.query("""SELECT value
                               FROM {}
@@ -57,7 +59,7 @@ def get_ids(sort_name):
                               GROUP BY *
                               ORDER BY time DESC
                               LIMIT 1
-                           """.format(sort_name)).raw
+                           """.format(measurement)).raw
 
     # Create dictionary of ID (value) and category (value)
     dict_ids = {}
@@ -74,6 +76,36 @@ def get_ids(sort_name):
     resorted_dict_ids = OrderedDict(reversed(list(sorted_dict_ids.items())))
 
     return resorted_dict_ids
+
+
+def get_pi_versions():
+    """Returns a dictionary of raspberry pi revisions and models"""
+    return {
+        'Beta':'1 Beta',
+        '0002':'1 Model B',
+        '0003':'1 Model B (ECN0001)',
+        '0004':'1 Model B',
+        '0005':'1 Model B',
+        '0006':'1 Model B',
+        '000d':'1 Model B',
+        '0003':'1 Model B',
+        '000f':'1 Model B',
+        '0007':'1 Model A',
+        '0008':'1 Model A',
+        '0009':'1 Model A',
+        '0010':'1 Model B+',
+        '0011':'Compute Mod',
+        '0012':'1 Model A+',
+        '0013':'1 Model B+',
+        '0014':'Computer Mod',
+        '0015':'1 Model A+',
+        'a21041':'2 Model B',
+        'a01041':'2 Model B',
+        '900092':'Zero',
+        '900093':'Zero',
+        'a02082':'3 Model B',
+        'a22082':'3 Model B',
+    }
 
 
 if __name__ == '__main__':
