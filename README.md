d74b42e437f62658ec06452aff5d1d8e2fe988ee
Kizniche <KyleTGabriel@gmail.com>
2016-05-28 22:49:51 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Mycodo Statistics Viewer
+
+This is not meant for public use. It is here to demonstrate how the statistics data is viewed and as a backup to preserve the source code in case of data loss.
+
+[![Mycodo Stats Viewer](http://kylegabriel.com/projects/wp-content/uploads/sites/3/2016/05/Mycodo-Stats-2016-05-28-22-47-41.png)](https://github.com/kizniche/mycodo_python)
---
+++ mycodo_stats_flask.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+import operator
+import os
+from collections import OrderedDict
+from flask import Flask, render_template, jsonify, request
+from flask.ext.influxdb import InfluxDB
+from influxdb import InfluxDBClient
+
+from config import INFLUXDB_USER
+from config import INFLUXDB_PASSWORD
+from config import INFLUXDB_DATABASE
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+
+app = Flask(__name__, template_folder=tmpl_dir)
+influx_db = InfluxDB(app)
+
+app.config['INFLUXDB_USER'] = INFLUXDB_USER
+app.config['INFLUXDB_PASSWORD'] = INFLUXDB_PASSWORD
+app.config['INFLUXDB_DATABASE'] = INFLUXDB_DATABASE
+
+@app.route('/', methods=('GET', 'POST'))
+def default_page():
+    sort_type = 'country'
+    if request.method == 'POST':
+        sort_type = request.form['sorttype']
+    stats_data = get_stats_data()
+    ids = get_ids(sort_type)
+
+    return render_template('mycodo_stats_view.html',
+                           sort_type=sort_type,
+                           stats_data=stats_data,
+                           ids=ids)
+
+
+def get_stats_data():
+    dbcon = influx_db.connection
+
+    raw_data = dbcon.query("""SELECT value
+                              FROM /.*/
+                              WHERE time > now() - 5d
+                              GROUP BY *
+                              ORDER BY time DESC
+                              LIMIT 1
+                           """).raw
+    return raw_data
+
+
+def get_ids(sort_name):
+    dbcon = influx_db.connection
+    raw_data = dbcon.query("""SELECT value
+                              FROM {}
+                              WHERE time > now() - 3d
+                              GROUP BY *
+                              ORDER BY time DESC
+                              LIMIT 1
+                           """.format(sort_name)).raw
+
+    # Create dictionary of ID (value) and category (value)
+    dict_ids = {}
+    for key, value in raw_data.iteritems():
+        for each_value in value:
+            dict_ids[each_value['tags']['anonymous_id']] = each_value['values'][0][1]
+
+    # Sort values (lowest to highest)
+    sorted_dict_ids = OrderedDict()
+    for key, value in sorted(dict_ids.iteritems(), key=lambda (k,v): (v,k)):
+        sorted_dict_ids[key] = value
+
+    # Reverse sorting of values (highest to lowest)
+    resorted_dict_ids = OrderedDict(reversed(list(sorted_dict_ids.items())))
+
+    return resorted_dict_ids
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Mycodo Flask HTTP server.",
+                                     formatter_class=argparse.RawTextHelpFormatter)
+
+    options = parser.add_argument_group('Options')
+    options.add_argument('-d', '--debug', action='store_true',
+                              help="Run Flask with debug=True (Default: False)")
+
+    args = parser.parse_args()
+
+    if args.debug:
+        debug=True
+    else:
+        debug=False
+
+    app.run(host='0.0.0.0', debug=debug)
